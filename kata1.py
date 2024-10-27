a30c3a782395a4e457d687b397662aa740cbc1b5
dethmet <mpgdethmet48@gmail.com>
2024-10-28 01:48:39 +0300

    feat: add solution

--- kata1.py
+++ kata1.py
@@ -1,15 +1,18 @@
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+list_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 number = int(input("vvedite chislo"))
 
 
-def function(x, a):
-    if a in x:
-        a_index = x.index(a)
+def get_calculation(numbers, num):
+    count = 0
+    if num not in numbers:
+        return int(-1)
     else:
-        a_index = -1
-    return a_index
+        for i in numbers:
+            if i != num:
+                count +=1
+            else:
+                return count
 
 
-a_index = function(list, number)
-print(a_index)
+print(get_calculation(list_numbers, number))
 
