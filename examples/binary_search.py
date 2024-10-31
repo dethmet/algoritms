c511693e36e1744c0ea0abb64c4db67a1551a68f
dethmet <mpgdethmet48@gmail.com>
2024-10-31 23:28:03 +0300

    fix: remove f s

--- examples/binary_search.py
+++ examples/binary_search.py
@@ -1,16 +1,3 @@
-def search_number(min, max, num):
-    searcher = (max + min) //2
-    count = 0
-    while num != searcher:
-        if num < searcher:
-            max = searcher
-            searcher = (searcher + min) // 2
-        else:
-            min = searcher
-            searcher = (max + searcher) //2
-        count += 1
-    return count
-
 
 def binary_search_iterative(arr, target):
     pass 
