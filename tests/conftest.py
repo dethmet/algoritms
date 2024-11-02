102be6693126590c0d65733060a1f3f92f4c354a
dethmet <mpgdethmet48@gmail.com>
2024-11-03 00:05:58 +0300

    test: edit fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,19 @@
+import random
+from string import ascii_lowercase, digits
+
 import pytest
 
 
 @pytest.fixture
 def integer_search_fixture():
-    numbers = [-3, 1, 3, 5, 10, 11, 15, 17, 21, 23]
-    orthodox_index = 4
+    numbers = sorted([random.randint(-100000,100000) for i in range(100000)])
+    orthodox_index = random.randint(0, len(numbers))
     existing_element = numbers[orthodox_index]
-    return numbers, existing_element, orthodox_index
+    return numbers, existing_element, numbers.index(existing_element)
 
 @pytest.fixture
 def value_search_fixture():
-    words = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-    orthodox_index = 4
+    words = sorted([''.join(random.choices(ascii_lowercase + digits, k=6)) for i in range(100000)])
+    orthodox_index = random.randint(0, len(words))
     existing_element = words[orthodox_index]
-    return words, existing_element, orthodox_index
+    return words, existing_element, words.index(existing_element)
