cec4d74b33e91878bd38cb1366d839edcc7dfff9
dethmet <mpgdethmet48@gmail.com>
2024-11-04 23:19:14 +0300

    fix: code binary_search

--- examples/binary_search.py
+++ examples/binary_search.py
@@ -3,12 +3,12 @@ def binary_search_iterative(arr, target):
     max_index = len(arr) - 1
     while min_index <= max_index:
         mid_index = (max_index + min_index) // 2
-        if target == arr[mid_index]:
-            return mid_index
+        if target < arr[mid_index]:
+            max_index = mid_index - 1
         elif target > arr[mid_index]:
             min_index = mid_index + 1
         else:
-            max_index = mid_index - 1
+            return mid_index
     return -1
 
 
@@ -20,9 +20,9 @@ def binary_search_recursive(arr, target, left=None, right=None):
     if left > right:
         return -1
     mid = (left + right) // 2
-    if target == arr[mid]:
-        return mid
-    elif target > arr[mid]:
+    if target > arr[mid]:
         return binary_search_recursive(arr, target, mid + 1, right)
-    return binary_search_recursive(arr, target, left, mid - 1)
+    elif target < arr[mid]:
+        return binary_search_recursive(arr, target, left, mid - 1)
+    return mid
     
